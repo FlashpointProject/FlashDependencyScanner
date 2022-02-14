37fd57e78748a709aee234e00e604d34197688e1
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2022-02-14 09:54:03 -0500

    Copy dependency jars to build on windows too.

--- build.bat
+++ build.bat
@@ -2,3 +2,6 @@ md build\classes
 md build\bin
 javac -cp lib\ffdec_lib.jar;lib\argparse4j-0.9.0.jar;lib\json-20211205.jar -d build\classes src\*.java src\swf\*.java
 jar cfm build\bin\flashdeps.jar manifest.txt -C build\classes src\
+copy lib\ffdec_lib.jar build\bin
+copy lib\argparse4j-0.9.0.jar build\bin
+copy lib\json-20211205.jar build\bin
