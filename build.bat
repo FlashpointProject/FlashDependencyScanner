332a8b401f6709b370e751001f1e1bca8a1e5cb7
LindirQuenya <53021080+LindirQuenya@users.noreply.github.com>
2022-02-14 11:47:22 -0500

    Create processedfiles.csv as empty, if it doesn't exist.

--- build.bat
+++ build.bat
@@ -5,3 +5,6 @@ jar cfm build\bin\flashdeps.jar manifest.txt -C build\classes src\
 copy lib\ffdec_lib.jar build\bin
 copy lib\argparse4j-0.9.0.jar build\bin
 copy lib\json-20211205.jar build\bin
+rem Little hack on the next line appends nothing to processedfiles.csv.
+rem I don't know why it works, but these people said it would: https://www.dostips.com/forum/viewtopic.php?t=3476
+rem. >> processedfiles.csv
--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ javac -cp "lib/ffdec_lib.jar:lib/argparse4j-0.9.0.jar:lib/json-20211205.jar" -d 
 jar cfm build/bin/flashdeps.jar manifest.txt -C build/classes src/
 # Copy the dependencies to the bin directory.
 cp lib/ffdec_lib.jar lib/argparse4j-0.9.0.jar lib/json-20211205.jar build/bin/
+# Create processedfiles.csv, if it doesn't exist already.
+touch processedfiles.csv
